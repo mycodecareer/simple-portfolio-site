f4697afe2865a3862d089305ba07a59f337601f6
Matthew Sevey <mjsevey@gmail.com>
2022-11-01 07:03:19 -0400

    Add template code

---
+++ js/consts.js
@@ -0,0 +1,13 @@
+// 1. Technology and Project
+const technology1 = "tech1";
+const projet1 = "proj1";
+
+// 2. Technology and Project
+const technology2 = "tech2";
+const projet2 = "proj2";
+
+// 3. Technology and Project
+const technology3 = "tech3";
+const projet3 = "proj3";
+
+export { technology1, proj1, technology2, proj2, technology3, proj3 };
---
+++ js/main.js
@@ -0,0 +1,4 @@
+import { technology1 } from "./consts.js";
+
+console.log("hello");
+console.log(technology1);
